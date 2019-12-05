bb03524d53eca3c2eaa3dd7c0e6168be6c9d5c23
doesDWQ <2354513578@qq.com>
2019-12-05 18:42:54 +0800

    将精简查询改为表单查询

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -17,37 +17,41 @@ class Log extends BaseModel
 
         $log_type = request('log_type'); //type值
 
-        $field = request('fields'); //搜索字段
+        $field = request('fields','_all'); //搜索字段
         $keyword = request('searchKey'); //搜索关键字
 
         $start = ($page-1)*$perPage;
 
-        $index = Helper_Function::$index;
-        //直接调用es api去精简查询
-        $url = "http://192.168.141.131:9200/{$index}/{$log_type}/_search?size={$perPage}&from={$start}";
+        $params = [
+            'from'=>$start,
+            'size'=> $perPage,
+            'index' => Helper_Function::$index,
+            'type' => $log_type,
+            'body' => [
+                'query' => [
+                    "match_all" => new \stdClass()
+                ]
+            ]
+        ];
 
-
-        //存在搜索字段就搜索
-        if(!empty($feild)){
-            $url .= "&q={$field}:{$keyword}";
-        }elseif(!empty($keyword)){
-            //搜索字段不存在的时候就去搜
-            $url .= "&q=*{$keyword}*";
+        //存在搜索字段的时候
+        if(!empty($keyword)){
+            $params['body'] =[
+                'query' => [
+                    'match' => [
+                        $field => $keyword
+                    ]
+                ]
+            ];
         }
 
-        //var_dump($url);die;
-        $data = file_get_contents($url);
-
-        $data = json_decode($data,true);
+        $data = Helper_Function::getEsClient()->search($params);
 
         $ret = [];
         foreach ($data['hits']['hits'] as $hit) {
-            //var_export($hit);die;
             $ret[] = $hit['_source'];
         }
-//        echo '<pre>';
-//
-//        var_export();die;
+
         $ret = static::hydrate($ret);
 
         $paginator = new LengthAwarePaginator($ret, $data['hits']['total'], $perPage);
